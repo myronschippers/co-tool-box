190d618759800552108e3fe94e201fa38385f6df
Myron <dubtwee@gmail.com>
2020-06-03 09:53:54 -0500

    adding the the company router and the start of the initial table structure

--- database/init.sql
+++ database/init.sql
@@ -7,3 +7,39 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+CREATE TABLE "employees" (
+    "id" SERIAL PRIMARY KEY,
+    "first_name" VARCHAR (100) NOT NULL,
+    "last_name" VARCHAR (100) NOT NULL,
+    "suffix" VARCHAR (10)
+);
+
+CREATE TABLE "pay_band" (
+    "id" SERIAL PRIMARY KEY,
+    "max_step",
+    "min_pay",
+    "max_pay"
+);
+
+-- RELATIONAL TABLES
+
+CREATE TABLE "position" (
+    "id" SERIAL PRIMARY KEY,
+    "position_title",
+    "pay_band_id"
+);
+
+CREATE TABLE "teams" (
+    "id" SERIAL PRIMARY KEY,
+    "team_name",
+    "manager_id"
+);
+
+-- JUNCTION TABLES
+
+CREATE TABLE "employees_position" ();
+
+CREATE TABLE "position_pay_band" ();
+
+CREATE TABLE "employees_teams" ();
